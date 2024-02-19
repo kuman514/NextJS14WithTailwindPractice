3b04ef3ca3fad20346e59964041152e96a78dd10
kumankoishi.py <7iggif@naver.com>
2024-02-19 23:56:49 +0900

    draft: add list getting handler

--- src/app/api/todos/routes.ts
+++ src/app/api/todos/routes.ts
@@ -1,7 +1,12 @@
-export const GET = async () => {
+export async function GET() {
   console.log('GET Request');
-};
 
-export const POST = async () => {
+  const response = await fetch('http://localhost:3030/todos');
+  const data = await response.json();
+
+  return Response.json({ data });
+}
+
+export async function POST() {
   console.log('POST Request');
-};
+}
