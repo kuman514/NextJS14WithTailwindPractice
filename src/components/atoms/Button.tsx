da56bff902db05f2f084bf2ada5eb6dfc7357c62
kuman514 <hadjadj0@gmail.com>
2024-03-06 07:13:00 +0900

    refactor: remove unused custom classname from button

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,19 +1,17 @@
 import React, { ReactNode } from 'react';
 
-const DEFAULT_CLASS_NAME =
-  'flex flex-row justify-center items-center gap-2 px-3 py-2 border border-slate-600 rounded-lg cursor-pointer hover:bg-lime-400';
-
 interface Props {
   children?: ReactNode;
   className?: string;
   onClick?(event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void;
 }
 
-export function Button({ children, className, onClick }: Props) {
-  const finalClassName = className ?? DEFAULT_CLASS_NAME;
-
+export function Button({ children, onClick }: Props) {
   return (
-    <button className={finalClassName} onClick={onClick}>
+    <button
+      className="flex flex-row justify-center items-center gap-2 px-3 py-2 border border-slate-600 rounded-lg cursor-pointer hover:bg-lime-400"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
