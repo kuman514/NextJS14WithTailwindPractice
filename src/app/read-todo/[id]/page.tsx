dba990abbbaa40b1d6d340510c0467197168fc49
kuman514 <hadjadj0@gmail.com>
2024-03-12 23:15:51 +0900

    draft: add buttons on read-todo & implement getting back

--- src/app/read-todo/[id]/page.tsx
+++ src/app/read-todo/[id]/page.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
+import { Button } from '^/components/atoms/Button';
 import { useTodoItem } from '^/hooks/use-todo-item';
 import { convertDateToString } from '^/utils/convert-date-to-string';
 
@@ -11,6 +14,7 @@ interface Props {
 
 export default function ReadTodo({ params }: Props) {
   const { todoItem } = useTodoItem(params.id);
+  const router = useRouter();
 
   return todoItem ? (
     <main className="w-full h-full flex flex-col justify-center items-center overflow-auto pt-4 gap-4">
@@ -18,6 +22,17 @@ export default function ReadTodo({ params }: Props) {
       <span>생성일자: {convertDateToString(todoItem.createdAt)}</span>
       <span>수정일자: {convertDateToString(todoItem.updatedAt)}</span>
       <span>{todoItem.detail}</span>
+      <div className="flex flex-row justify-centers gap-4">
+        <Button
+          onClick={() => {
+            router.back();
+          }}
+        >
+          이전
+        </Button>
+        <Button onClick={() => {}}>수정하기</Button>
+        <Button onClick={() => {}}>삭제하기</Button>
+      </div>
     </main>
   ) : null;
 }
